6e7ae25286a57978ab71e6fe0dba812f86145588
adenuga emmanuel <ehmmygal@gmail.com>
2025-11-22 04:41:34 -1200

    sec commit

--- server/server.js
+++ server/server.js
@@ -10,7 +10,7 @@ const app = express();
 
 // CORS Setup
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:5173', 
+  origin: process.env.FRONTEND_URL || 'http://nuel-house.vercel.app', 
   credentials: false
 }));
 
